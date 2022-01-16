2a7507723b9a3b7af41116c0a5231ba82ed231c7
miguelpombodev <mp96dev@gmail.com>
2022-01-16 16:57:42 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps /*, AppContext */ } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'styles/global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="" />
+        <meta name="description" content="Simples NextJS app" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
